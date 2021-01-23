f7bf8f2d84af3e79a03e1de02be970218952b019
BlakeVidebeck <blakevidebeck@gmail.com>
2021-01-23 22:48:16 +1300

    add header component and shop, contact link

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css'
 
 import HomeScreen from './screens/homescreen/HomeScreen'
 import ShopScreen from './screens/shopscreen/ShopScreen'
+import Header from './components/header/Header'
 
 function App() {
 	return (
 		<>
+			<Header />
 			<Switch>
 				<Route exact path='/shop' component={ShopScreen} />
 				<Route exact path='/' component={HomeScreen} />
