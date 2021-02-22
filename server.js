b9eb43100a9befe7d077eb98f7a17bedd785f7e2
BlakeVidebeck <blakevidebeck@gmail.com>
2021-02-23 10:12:01 +1300

    add variable for __dirname

--- server.js
+++ server.js
@@ -19,6 +19,8 @@ app.use(express.json())
 
 app.use(cors())
 
+const __dirname = path.resolve()
+
 if (process.env.NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '/client/build')))
 	app.get('*', (req, res) =>
